3853db023251a4c4467657fabfb3eee363ad5da4
Paco Alba <kiviev@gmail.com>
2017-04-15 18:28:20 +0200

    Funcion ssh para blink y para pi local

--- myFunctions/.myFunctions.sh
+++ myFunctions/.myFunctions.sh
@@ -1,2 +1,22 @@
 
 source $CONF_DIR/myFunctions/blink.sh
+
+# my functions
+
+function mssh()
+{
+USER=''
+IP=''
+if [ $1 = '-p' ]; then
+	USER='pi'
+	IP='192.168.1.120'
+elif [ $1 = '-b' ]; then
+	USER='root'
+	IP='192.168.56.2'
+else
+	echo 'no has indicado argumento'
+	return
+fi
+echo "ssh $USER@$IP"
+	ssh $USER@$IP
+}
--- myFunctions/blink.sh
+++ myFunctions/blink.sh
@@ -1,14 +1,4 @@
 
-function ssh_blink(){
-	if [ "$1" != '' ]; then
-		echo "ssh $1"
-		ssh "$1"
-	else
-		echo "ssh root@192.168.56.2"
-		ssh root@192.168.56.2
-	fi
-}
-
 function blgo(){
 	bl4 && go $1
 	bl && go $1
