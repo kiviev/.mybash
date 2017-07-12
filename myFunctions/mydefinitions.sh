e8ef7b50924e669f97e941aa88514634df56eeda
Paco Alba <kiviev@gmail.com>
2017-07-12 16:24:25 +0200

    variables colores

---
+++ myFunctions/mydefinitions.sh
@@ -0,0 +1,32 @@
+
+# colors
+BLACK='\033[0;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+
+DARK_GRAY='\033[1;30m'
+LIGHT_RED='\033[1;31m'
+LIGHT_GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHT_BLUE='\033[1;34m'
+LIGHT_PURPLE='\033[1;35m'
+LIGHT_CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+# no color
+NC='\033[0m'
+
+
+
+# BLACK        0;30     DARK GRAY     1;30
+# RED          0;31     LIGHT RED     1;31
+# GREEN        0;32     LIGHT GREEN   1;32
+# BROWN/ORANGE 0;33     YELLOW        1;33
+# BLUE         0;34     LIGHT BLUE    1;34
+# PURPLE       0;35     LIGHT PURPLE  1;35
+# CYAN         0;36     LIGHT CYAN    1;36
+# LIGHT GRAY   0;37     WHITE         1;37
