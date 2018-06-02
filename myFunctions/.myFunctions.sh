dda1dff7bf69a6119b6de2eacd56442f2e5095f7
Paco Alba <kiviev@gmail.com>
2018-06-02 03:08:43 +0200

    nuevas funciones

--- myFunctions/.myFunctions.sh
+++ myFunctions/.myFunctions.sh
@@ -4,24 +4,43 @@ source $CONF_DIR/myFunctions/mydefinitions.sh
 
 # my functions
 
-function mssh()
-{
-USER=''
-IP=''
-if [ $1 = '-p' ]; then
-	USER='pi'
-	IP='192.168.1.120'
-elif [ $1 = '-b' ]; then
-	USER='root'
-	IP='192.168.56.2'
-else
-	echo 'no has indicado argumento'
-	return
-fi
-echo "ssh $USER@$IP"
-	ssh $USER@$IP
+function mssh(){
+	USER=''
+	IP=''
+	ARGS=''
+	if [ $1 = 'pi' ]; then
+		USER='pi'
+		IP='192.168.1.140'
+	elif [ $1 = '-b' ]; then
+		USER='root'
+		IP='192.168.56.2'
+	elif [ $1 = 'ofertaka' ]; then
+		USER='bitnami'
+		IP='34.241.115.147'
+		ARGS=' -i ~/.ssh/ofertaka.pem '
+	else
+		echo 'no has indicado argumento o no es valido'
+		return
+	fi
+	echo "ssh $ARGS$USER@$IP"
+	ssh $ARGS$USER@$IP
 }
 
+function sshtunel(){
+	USER=''
+	IP=''
+	ARGS=''
+	if [ $1 = '-ofertaka' ]; then
+		USER='bitnami'
+		IP='34.241.115.147'
+		ARGS='-N -L 8888:127.0.0.1:80 -i ~/.ssh/ofertaka.pem '
+	else
+		echo 'no has indicado argumento'
+		return
+	fi
+	echo "ssh $ARGS$USER@$IP"
+	ssh $ARGS$USER@$IP
+}
 
 
 function printText(){
